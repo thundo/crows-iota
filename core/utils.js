44c9050998b317dc15850e3f21112c41f0f145e4
Diego Ferri <ferri.diego@gmail.com>
2019-08-09 11:30:07 +0200

    Core package.

---
+++ core/utils.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {asciiToTrytes} = require('@iota/converter');
+const depth = 3;
+const minWeightMagnitude = 9;
+
+const sendZeroValueTx = async (provider, seed, recipient, data) => {
+    const transfers = [{
+        value: 0,
+        address: recipient,
+        tag: 'CROWS',
+        message: asciiToTrytes(JSON.stringify(data)),
+    }];
+    const trytes = await provider.prepareTransfers(seed, transfers, {});
+    const bundle = await provider.sendTrytes(trytes, depth, minWeightMagnitude);
+    console.log(`Published transaction with tail hash: ${bundle[0].hash}`);
+    return bundle[0].hash;
+};
+
+module.exports = {
+    sendZeroValueTx,
+};
